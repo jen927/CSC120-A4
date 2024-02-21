4c245b0e0189222e054e8744b29834840969d2de
jen927 <157835344+jen927@users.noreply.github.com>
2024-02-20 22:15:35 -0500

    constructed the engine class

--- Engine.java
+++ Engine.java
@@ -1,3 +1,35 @@
 public class Engine {
+    //attributes
+    FuelType f;
+    double currentFuelLevel;
+    double maxFuelLevel;
 
+    //constructor
+    public Engine(FuelType F, double currentFuelLevel) {
+        this.currentFuelLevel = currentFuelLevel;
+        maxFuelLevel = 100.0;
+    }
+
+    // refuel method
+    public void refuel() {
+        currentFuelLevel = maxFuelLevel;
+    }
+
+    // go method
+    public boolean go() {
+        while (currentFuelLevel > 0) {
+            currentFuelLevel -= 10;
+            System.out.println(currentFuelLevel);
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Engine myEngine = new Engine(FuelType.ELECTRIC, 100.0);
+        while (myEngine.go()) {
+            System.out.println("Choo choo!");
+        }
+        System.out.println("Out of fuel.");
+    }
 }
