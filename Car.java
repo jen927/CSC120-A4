717325e2d6f5966db6cdb5436533a5eca0b66be5
jen927 <157835344+jen927@users.noreply.github.com>
2024-02-25 21:37:02 -0500

    added constructor and two methods to Car class

--- Car.java
+++ Car.java
@@ -1,3 +1,30 @@
+import java.util.ArrayList;
+
 public class Car {
+    // attributes
+    ArrayList passengers;
+    int maxPassengers;
+
+    //constuctor
+    //  a constructor, which takes in an initial value
+    //  for the `Car`'s maximum capacity and
+    //  initializes an appropriately-sized `ArrayList`
+    public Car(int maxPassengers) {
+        ArrayList<String> passengers = new ArrayList<String>();
+        this.maxPassengers = maxPassengers;
+    }
+    // accessor-like methods `public int getCapacity()`
+    // and `public int seatsRemaining()`
+    // that return the maximum capacity and
+    // remaining seats, respectively
+    public int getCapacity() {
+        return maxPassengers;
+    }
+
+    public int seatsRemaining() {
+        return maxPassengers - passengers.size();
+    }
+
+    
 
 }
